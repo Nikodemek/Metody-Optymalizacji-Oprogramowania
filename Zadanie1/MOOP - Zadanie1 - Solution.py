aa5fb709bd651e31227a4ea5e66c987ee26c330f
Nikodem Kirsz <nikodemkirsz101@gmail.com>
2025-04-03 22:17:39 +0200

    restructurization

--- Zadanie1/MOOP - Zadanie1 - Solution.py
+++ Zadanie1/MOOP - Zadanie1 - Solution.py
@@ -1,99 +1,13 @@
 from __future__ import annotations
 
-import math
-from enum import Enum
 from typing import Dict, Callable
-
 import matplotlib.pyplot as plt
-from Common.moop_gui import initialize_app, TaskInput, InputType
 
-class Axis(Enum):
-  X = 1
-  Y = 2
-
-class Limitation(Enum):
-  LESS_OR_EQUAL = 1
-  GREATER_OR_EQUAL = 2
-  EQUAL = 3
-
-class Point2d:
-  def __init__(self, x: float, y: float):
-    self.x = x
-    self.y = y
-
-  def magnitude_from(self, other: Point2d):
-    return math.sqrt((self.x - other.x) ** 2 + (self.y - other.y) ** 2)
-
-  def distance_from(self, other: Point2d):
-    return math.sqrt(self.magnitude_from(other))
-
-  def __repr__(self):
-    return f"[{self.x:.3g}, {self.y:.3g}]"
-
-class LinearFunction:
-  def __init__(self, slope_a: float, slope_b: float, gen_a: float, gen_b: float, gen_c: float):
-    self.slope_a = slope_a
-    self.slope_b = slope_b
-    self.gen_a = gen_a
-    self.gen_b = gen_b
-    self.gen_c = gen_c
-
-  def calculate_value(self, x):
-    return self.slope_a * x + self.slope_b
-
-  def find_intersection(self, other: LinearFunction):
-    if self.slope_a == other.slope_a:
-      if self.slope_b != other.slope_b:
-        return None
-      else:
-        return 0
-
-    a_diff = self.slope_a - other.slope_a
-    intersection = (other.slope_b - self.slope_b) / a_diff
-
-    return intersection
-
-  def find_intersection_with_axis(self, axis: Axis):
-    match axis:
-      case Axis.X:
-        return self.gen_c / self.gen_a
-      case Axis.Y:
-        return 0
-
-  def is_rising(self) -> bool:
-    return self.slope_a > 0
-
-  @classmethod
-  def from_slope(cls, a: float, b: float):
-    if a == 0:
-      raise ValueError("Slope A must not be 0!")
-
-    gen_a = -a
-    gen_b = 1
-    gen_c = b
-
-    func = cls(a, b, gen_a, gen_b, gen_c)
-    return func
-
-  @classmethod
-  def from_general(cls, gen_a: float, gen_b: float, gen_c: float):
-    if gen_b == 0:
-      raise ValueError("Constant b can not be 0!")
-
-    a = -gen_a / gen_b
-    b = gen_c / gen_b
-
-    func = cls(a, b, gen_a, gen_b, gen_c)
-    return func
-
-  def to_slope_string(self):
-    sign = '+' if self.slope_b >= 0 else '-'
-    return f'y = {self.slope_a}x {sign} {math.fabs(self.slope_b)}'
-
-  def to_general_string(self):
-    sign = '+' if self.gen_b >= 0 else '-'
-    return f'{self.gen_a}x {sign} {math.fabs(self.gen_b)}y = {self.gen_c}'
-
+from common.linearfunction import LinearFunction
+from common.moop_gui import initialize_app, TaskInput, InputType
+from common.points2d import Point2d
+from common.axis import Axis
+from common.limitation import Limitation
 
 def find_all_notable_points(funcs: list[LinearFunction]) -> list[Point2d]:
   points: list[Point2d] = []
@@ -207,11 +121,11 @@ def calculate(value_getters: Dict[str, Callable[[], float]]):
     (func_h, Limitation.LESS_OR_EQUAL),
     (func_c, Limitation.LESS_OR_EQUAL),
   ])
-  display_range = get_range_to_display(points, 0.3)
   optimal_points = find_optimal(valid_points, utility_func)
 
   plt.figure(figsize=(8, 6))
 
+  display_range = get_range_to_display(points, 0.3)
   plot_func(func_h, display_range, 'purple')
   plot_func(func_c, display_range, 'green')
 
@@ -233,7 +147,7 @@ def calculate(value_getters: Dict[str, Callable[[], float]]):
   plt.grid()
 
   print(
-    f'Punkty, w których rozwiązanie jest optymalne: {optimal_points}),\n'
+    f'Punkty, w których rozwiązanie jest optymalne: {optimal_points}, '
     f'ponieważ zysk w tych punktach wynosi kolejno {[utility_func(point.x, point.y) for point in optimal_points]}'
   )
 
@@ -243,14 +157,14 @@ def calculate(value_getters: Dict[str, Callable[[], float]]):
 initialize_app(
   title="Zadanie 1 (zad6)",
   inputs = [
-    TaskInput('S1_H', 'Wykorzystanie H przy S1', InputType.INT, -2),
-    TaskInput('S2_H', 'Wykorzystanie H przy S2', InputType.INT, 1),
-    TaskInput('S1_C', 'Wykorzystanie C przy S1', InputType.INT, 1),
-    TaskInput('S2_C', 'Wykorzystanie C przy S2', InputType.INT, 2),
-    TaskInput('S1_zysk', 'Zysk z S1', InputType.INT, 3),
-    TaskInput('S2_zysk', 'Zysk z S2', InputType.INT, 2),
-    TaskInput('H_max', 'Max zużycie H', InputType.INT, 2),
-    TaskInput('C_max', 'Max zużycie C', InputType.INT, 8),
+    TaskInput('S1_H', 'Wykorzystanie H przy S1', InputType.FLOAT, -2),
+    TaskInput('S2_H', 'Wykorzystanie H przy S2', InputType.FLOAT, 1),
+    TaskInput('S1_C', 'Wykorzystanie C przy S1', InputType.FLOAT, 1),
+    TaskInput('S2_C', 'Wykorzystanie C przy S2', InputType.FLOAT, 2),
+    TaskInput('S1_zysk', 'Zysk z S1', InputType.FLOAT, 3),
+    TaskInput('S2_zysk', 'Zysk z S2', InputType.FLOAT, 2),
+    TaskInput('H_max', 'Max zużycie H', InputType.FLOAT, 2),
+    TaskInput('C_max', 'Max zużycie C', InputType.FLOAT, 8),
   ],
   apply=calculate,
 )
